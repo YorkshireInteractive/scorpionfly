6f2f70a63adfd9f96519acf9a2f4e4a45fbbf792
Jason Bennett <minusbox@gmail.com>
2016-09-21 14:19:21 -0700

    fixed another PR review and some content updates.

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,6 @@ var $navLinks = $navigation.find('.header-nav-list-item-link:not(.hire-us-link)'
 var $topBar = $('.top-bar');
 var $colorChangeElements = $navLinks.add($topBar);
 
-var buffer = 50;
 var splashTransitionHeight;
 var secondTransitionHeight;
 var thirdTransitionHeight;
@@ -16,46 +15,13 @@ var thirdTransitionHeight;
  */
 var getTransitionHeights = function () {
   splashTransitionHeight = $('.big-splash').height();
-  secondTransitionHeight = splashTransitionHeight + $('#who-we-are').height();
-  thirdTransitionHeight = secondTransitionHeight + $('#what-we-do').height();
 };
-
-/**
- * Sets element passed to it to active and removes active class from sibling nav
- */
-var setActiveNav = function ($el) {
-  $el.addClass('active').parent().siblings().find('.header-nav-list-item-link').removeClass('active');
-};
-
-/**
- * Scroll animates page to Ycoord
- */
-var animateScroll = function (Ycoord) {
-  $htmlBody.animate({ scrollTop: Ycoord+"px" });
-};
-
-/**
- * Figures out which type of event to use. We don't want to use click on mobile
- * because there's a 300ms delay as it waits for a doubleclick.
- */
-var getClickEvent = function () {
-  return 'ontouchstart' in document.documentElement ? 'touchend' : 'click';
-};
-
 
 $window.resize(function () {
   // refetch transition heights on resize as well
   getTransitionHeights();
 }).scroll(function () {
-  var s = window.scrollY;
-
-  if (s >= splashTransitionHeight && s < secondTransitionHeight) {
-    $colorChangeElements.addClass('after-fold');
-  }
-  else if (s >= secondTransitionHeight && s < thirdTransitionHeight) {
-    $colorChangeElements.addClass('after-fold');
-  }
-  else if (s >= thirdTransitionHeight) {
+  if (window.scrollY >= splashTransitionHeight) {
     $colorChangeElements.addClass('after-fold');
   }
   else {
@@ -74,11 +40,9 @@ getTransitionHeights();
   });
 
   $(".btn-hire-us").click(function() {
-    $('#contact-form-modal').fadeToggle();
-    $('#hire-us-wrapper').fadeToggle();
+    $('#contact-form-modal, #hire-us-wrapper').fadeToggle();
 
   });
   $(".modal-dismiss-btn").click(function() {
-    $('#contact-form-modal').fadeToggle();
-    $('#hire-us-wrapper').fadeToggle();
+    $('#contact-form-modal, #hire-us-wrapper').fadeToggle();
   });
